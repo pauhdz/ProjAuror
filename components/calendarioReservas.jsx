1cbe05cce7ad2c9ed0d79f4cddfff19e2f09ae1c
Pau Hernandez <pauhergue@campus.monlau.com>
2024-05-14 19:09:47 +0200

    nuevo form activitats

--- components/calendarioReservas.jsx
+++ components/calendarioReservas.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from "@fullcalendar/interaction";
 import FormActivitats from './formActivitats';
 import Modal from 'react-modal';
 import Button from 'react-bootstrap/Button';
+import { Transition } from '@headlessui/react';
+import { Dialog } from '@mui/material';
 
 export default function Calendar() {
     const [calendarView] = useState('dayGridMonth');
     const [showDialog, setShowDialog] = useState(false);
     const [selectedDate, setSelectedDate] = useState(null);
     const [events, setEvents] = useState([]);
+    const [showEventDialog, setShowEventDialog] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [actividades, setActividades] = useState([]);
 
 
     const handleDateClick = (info) => {
@@ -24,18 +29,36 @@ export default function Calendar() {
     };
 
     const handleEventClick = (info) => {
-        const eventEl = info.el;
-        const rect = eventEl.getBoundingClientRect();
-        setContextMenuVisible(true);
-        setContextMenuPosition(rect);
+        setSelectedEvent(info.event);
+        setShowEventDialog(true);
     };
 
+    const handleOutsideClick = (event) => {
+        if (event.target.classList.contains('fixed')) {
+            setShowDialog(false);
+            setShowEventDialog(false);
+        }
+    };
+
+    const handleInfoClick = (activity) => {
+        setShowDialog(true);
+    };
+
+    const eliminarActividad = (id) => {
+        const nuevasActividades = actividades.filter(actividad => actividad.id !== id);
+        setActividades(nuevasActividades);
+    };
+
+
     return (
-        <div className='p-20'>
+        <div className='p-20' onClick={handleOutsideClick}>
             <div className='bg-white p-4 rounded-md shadow-2xl'>
                 <FullCalendar
                     plugins={[dayGridPlugin, interactionPlugin]}
                     initialView={calendarView}
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
                     timeZone='local'
                     height="700px"
                     headerToolbar={{
@@ -53,8 +76,45 @@ export default function Calendar() {
                     eventClick={handleEventClick}
                 />
 
-                {showDialog && <FormActivitats onClose={() => setShowDialog(false)} selectedDate={selectedDate} onSubmit={handleEventSubmit} />}
+                <Transition
+                    show={showDialog}
+                    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+                    style={{ zIndex: 9999 }}
+                    enter="transition-opacity duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition-opacity duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <FormActivitats
+                        onClose={() => setShowDialog(false)}
+                        selectedDate={selectedDate}
+                        onSubmit={handleEventSubmit}
+                        eventData={selectedEvent} // Pasar los datos del evento al formulario
+                    />
+                </Transition>
+
+                <Transition
+                    show={showEventDialog}
+                    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+                    style={{ zIndex: 9999 }}
+                    enter="transition-opacity duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition-opacity duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className='bg-white p-10 rounded-xl items-center'>
+                        <button className='border-2 border-blue-400 rounded-xl p-4 text-blue-400 font-bold mr-4 min-w-6 w-32 hover:bg-blue-400 hover:text-white' onClick={() => { setShowEventDialog(false); handleInfoClick(selectedEvent); }}>Informació</button>
+                        <button className='border-2 border-orange-400 rounded-xl p-4 text-orange-400 font-bold mr-4 min-w-6 w-32 hover:bg-orange-400 hover:text-white' onClick={() => { setShowEventDialog(false); setShowDialog(true); }}>Editar</button>
+                        <button className='border-2 border-red-600 rounded-xl p-4 text-red-600 font-bold min-w-6 w-32 hover:bg-red-600 hover:text-white' onClick={() => {eliminarActividad(selectedEvent.id); setShowEventDialog(false);}}>Eliminar</button>
+                    </div>
+                </Transition>
+
             </div>
+
         </div>
     );
 }
--- components/formActivitats.jsx
+++ components/formActivitats.jsx
@@ -1,4 +1,4 @@
-const FormActivitats = ({ onClose, selectedDate, onSubmit }) => {
+const FormActivitats = ({ onClose, selectedDate, onSubmit, eventData }) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -27,7 +27,7 @@ const FormActivitats = ({ onClose, selectedDate, onSubmit }) => {
             <div class="flex flex-col space-y-4 rounded-lg shadow-2xl py-6 px-14 bg-white">
                 <label for="nombreActividad" class="text-lg">Nom de la Activitat:</label>
                 <input type="text" id="nombreActividad" name="nombreActividad" required
-                    class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-gray-800 border-b" />
+                    class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-gray-800 border-b" defaultValue={eventData ? eventData.title : ''}/>
 
                 <div className="flex gap-10 items-center mb-4">
                     <label for="fechaActividad" class="text-lg">Data de la Activitat:</label>
@@ -38,16 +38,16 @@ const FormActivitats = ({ onClose, selectedDate, onSubmit }) => {
                     <label for="rangoHora" class="text-lg ml-10">Horari de la Activitat:</label>
                     <div class="flex items-center">
                         <input type="time" id="rangoHoraInicio" name="rangoHoraInicio" required
-                            class="border border-gray-300 rounded-md px-3 py-2 mr-2 focus:outline-none focus:border-gray-800 border-b" />
+                            class="border border-gray-300 rounded-md px-3 py-2 mr-2 focus:outline-none focus:border-gray-800 border-b" defaultValue={eventData ? eventData.start : ''} />
                         <span class="text-lg">-</span>
                         <input type="time" id="rangoHoraFin" name="rangoHoraFin" required
-                            class="border border-gray-300 rounded-md px-3 py-2 ml-2 focus:outline-none focus:border-gray-800 border-b" />
+                            class="border border-gray-300 rounded-md px-3 py-2 ml-2 focus:outline-none focus:border-gray-800 border-b" defaultValue={eventData ? eventData.end : ''}/>
                     </div>
                 </div>
 
                 <label for="personasActividad" class="text-lg">Persones de la Activitat:</label>
                 <input type="number" id="personasActividad" name="personasActividad" required
-                    class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-gray-800 border-b" />
+                    class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-gray-800 border-b" defaultValue={eventData ? eventData.numPersonas : ''} />
 
                 <label for="descripcionActividad" class="text-lg">Descripció de la Activitat:</label>
                 <textarea id="descripcionActividad" name="descripcionActividad" rows="4"
